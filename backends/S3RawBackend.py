ea733e415d398bdd1bd64618bd986529c827b053
gary hurtz <garyhurtz@gmail.com>
2016-06-16 12:12:05 +0800

    Resolved Unicode issue in S3RawBackend, improved tests

--- backends/S3RawBackend.py
+++ backends/S3RawBackend.py
@@ -2,7 +2,7 @@
 from S3BaseBackend import S3BaseBackend
 from boto.exception import S3ResponseError
 from boto.s3.key import Key
-from io import StringIO
+from StringIO import StringIO
 import gzip
 
 
@@ -48,6 +48,7 @@ class S3RawBackend(S3BaseBackend):
         if mimetype not set attempt to extract content_type from the file
 
         by default the file will be private, set public to make public
+
         :param key: the key to store the object under
         :param fp_or_str: file pointer or string
         :param public: whether the object should me made public
@@ -66,37 +67,33 @@ class S3RawBackend(S3BaseBackend):
         # get the contents to store
         content = fp_or_str
 
-        # if it is a filepoint, read the content
+        # if it is a file-pointer, read the content
         if isinstance(content, file):
             content = content.read()
 
+        # if mimetype not forced get from fp
+        if mimetype is None and hasattr(fp_or_str, u'content_type'):
+            mimetype = fp_or_str.content_type
+
+        headers[u'Content-Type'] = mimetype
+
+        if cache:
+            # the value must be a byte string or else URL encoding goofs it up
+            headers[u'Cache-Control'] = u'public, max-age={0}'.format(cache)
+
+        if mimetype in self.COMPRESSIBLE:
+            print u'compressing ', key
+            headers[u'Content-Encoding'] = u'gzip'
+
+            sio = StringIO()
+            gzf = gzip.GzipFile(fileobj=sio, mode=u'wb')
+            gzf.write(content)
+            gzf.close()
+
+            # Output gzipped stream.
+            content = sio.getvalue()
+
         try:
-
-            # if mimetype not forced get from fp
-            if mimetype is None and hasattr(fp_or_str, u'content_type'):
-                mimetype = fp_or_str.content_type
-
-            headers[u'Content-Type'] = mimetype
-
-            if cache:
-                # the value must be a byte string or else URL encoding goofs it up
-                headers[u'Cache-Control'] = u'public, max-age={0}'.format(cache)
-
-            if mimetype in self.COMPRESSIBLE:
-                print u'compressing ', key
-                headers[u'Content-Encoding'] = u'gzip'
-
-                sio = StringIO()
-                gzf = gzip.GzipFile(fileobj=sio, mode='wb')
-                gzf.write(content)
-                gzf.close()
-
-                # Output gzipped stream.
-                content = sio.getvalue()
-
-            # else:
-            #     content = fp.read()
-
             k.set_contents_from_string(content, headers=headers, policy=policy)
 
         except Exception as e:
--- tests/test_s3_raw_backend.py
+++ tests/test_s3_raw_backend.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*- #
 import unittest
 from backends.S3RawBackend import S3RawBackend
-from tests.RawBackendTests import RawBackendTests
 
 
-class TestS3RawBackend(unittest.TestCase, RawBackendTests):
+class TestS3Basic(unittest.TestCase):
 
     def setUp(self):
         self.dut = S3RawBackend(u'test-stellour')
@@ -14,3 +13,108 @@ class TestS3RawBackend(unittest.TestCase, RawBackendTests):
 
     def tearDown(self):
         self.dut.clear()
+
+    def test_lump(self):
+        """
+        Since it takes time to upload and clear the object for each
+        test, just do all the basics in one run
+
+        :param key:
+        :return:
+        """
+        self.assertTrue(u'test' in self.dut)
+        self.assertTrue(u'test' in self.dut.keys())
+        self.assertIsInstance(self.dut.load(u'test'), basestring)
+
+        before = len(self.dut)
+        self.assertTrue(self.dut.delete(u'test'))
+        self.assertEqual(1, before - len(self.dut))
+
+
+class TestS3RawBackend(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.dut = S3RawBackend(u'test-stellour')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.dut.clear()
+
+    def execute(self, key, original):
+
+        before = len(self.dut)
+        result = self.dut.dump(key, original)
+
+        self.assertTrue(result)
+        self.assertEqual(1, len(self.dut) - before)
+
+        # confirm no errors on load
+        self.dut.load(key)
+
+    @unittest.expectedFailure
+    def test_is_gzipped(self):
+        """
+        Need a clean way to confirm that the file was gzipped
+        :return:
+        """
+        raise NotImplementedError()
+
+    def test_dump_css_fp(self):
+        """
+        Store a value in the backend
+        """
+        key = u'css_fp'
+
+        with open(u'main.css', u'r') as value:
+            self.execute(key, value)
+
+    def test_dump_css_string(self):
+        """
+        Store a value in the backend
+        """
+        key = u'css_string'
+
+        with open(u'main.css', u'r') as infile:
+            value = infile.read()
+
+        self.execute(key, value)
+
+    def test_dump_png_fp(self):
+        """
+        Store a value in the backend
+        """
+        key = u'png_fp'
+
+        with open(u'test.png', u'r') as value:
+            self.execute(key, value)
+
+    def test_dump_png_as_string(self):
+        """
+        Store a value in the backend
+        """
+        key = u'png_string'
+
+        with open(u'test.png', u'r') as infile:
+            value = infile.read()
+
+        self.execute(key, value)
+
+    def test_dump_ascii_string(self):
+        """
+        Store a value in the backend
+        """
+        key = u'ascii'
+        value = 'this is a test string'
+
+        self.execute(key, value)
+
+    def test_dump_unicode_string(self):
+        """
+        Store a value in the backend
+        """
+        key = u'中文'
+        value = u'一点点中文'
+
+        self.execute(key, value)
+
